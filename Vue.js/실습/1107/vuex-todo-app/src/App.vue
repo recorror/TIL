de9888f48e81bb35459f4b2bf814991d42a22a0e
recorror <itgwd9701@gmail.com>
2022-11-07 18:04:24 +0900

    123213

---
+++ Vue.js/실습/1107/vuex-todo-app/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>TodoList</h1>
+    <h2>모든 Todo 개수: {{ allTodosCount }}</h2>
+    <h2>완료된 todo 수: {{ completedTodosCount }}</h2>
+    <h2>미완료 todo 수: {{ unCompletedTodosCount }}</h2>
+    <TodoList/>
+    <TodoForm/>
+    <!-- <button @click="loadTodos">Todo 불러오기</button> -->
+  </div>
+</template>
+
+<script>
+import TodoForm from '@/components/TodoForm'
+import TodoList from '@/components/TodoList'
+export default {
+  name: 'App',
+  components: {
+    TodoForm,
+    TodoList,
+  },
+  computed: {
+    allTodosCount(){
+      return this.$store.getters.allTodosCount
+    },
+    completedTodosCount(){
+      return this.$store.getters.completedTodosCount
+    },
+    unCompletedTodosCount(){
+      return this.$store.getters.unCompletedTodosCount
+    }
+  },
+  methods: {
+    loadTodos() {
+      this.$store.dispatch('loadTodos')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue.js/실습/1107/vuex-todo-app/src/components/TodoForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text"
+      v-model.trim="todoTitle"
+      @keyup.enter="createTodo"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  data(){
+    return {
+      todoTitle:null,
+    }
+  },
+  methods: {
+    createTodo(){
+      // console.log(this.todoTitle)
+      this.$store.dispatch('createTodo', this.todoTitle)
+      this.todoTitle=null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
